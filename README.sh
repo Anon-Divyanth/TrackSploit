b8f78940584605753a9453b70341f236ec79ffbb
Anon-Divyanth <69021670+Anon-Divyanth@users.noreply.github.com>
2020-12-20 06:18:34 +0530

    Update README.sh

--- README.sh
+++ README.sh
@@ -1,1 +1,26 @@
-d
+# Introduction:
+It is a Advance Level of Tracking tool. Track
+Phone number location,Ip Address location and 
+track mac address location. And gather website info
+With Whois Lookup, get domain name system with 
+DNS lookup, and find open ports of a website using 
+Nmap Port scanner, get all subdomains of a website
+using subdomains scanner, get the path of your
+Packets with Tracerote, and generate "52" types
+of hashes. And mainly gather YouTube video information
+And download YouTube videos with youtube downloader.
+
+# Installation:
+
+**pkg install python**
+
+**git clone https://github.com/Anon-Divyanth/TrackSploit**
+
+**cd TrackSploit**
+
+**bash install.sh**
+
+**python tracksploit.py**
+
+# Thanks to:
+ **Vinay**,**Rokuru Hamasaki**,**Pratap**,**Folorunsho Muiz**,**varun**,**Prudhvi**,**yogesh**,**Raj Kumar**
